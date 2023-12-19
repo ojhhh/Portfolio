241fea9b4ad42790541a7a64e05fd6c72e2341cf
ojhhh <winnel@naver.com>
2023-12-19 21:01:33 +0900

    portfolio 수정중

--- src/contents/education.jsx
+++ src/contents/education.jsx
@@ -23,6 +23,44 @@ const Education = () => {
                     </ul>
                 </li>
             </ul>
+            <ul className="flex flex-col divide-y divide-gray-400">
+                <li className="flex flex-row gap-[32px] py-[32px">
+                    <h3 className="text-[16px] md:text-[24px] font-extrabold md:font-semibold w-[50%] basisc-[25%]">
+                        AWS를 활용한 클라우드 응용 SW 개발자 양성
+                    </h3>
+                    <span></span>
+                    <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
+                                <li>[ 웹 프로그래밍 기초 ] : HTML, CSS, jQuery</li>
+                                <li>[ 백엔드 ] : Java, Java Spring</li>
+                                <li>[ 프론트엔드 ] : JSP</li>
+                                <li>[ 데이터베이스 ] : Oracle</li>
+                                <li>[ ETC ] : AWS EC2, AWS lambda, AWS DynamoDB</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <ul className="flex flex-col divide-y divide-gray-400">
+                <li className="flex flex-row gap-[32px] py-[32px">
+                    <h3 className="text-[16px] md:text-[24px] font-extrabold md:font-semibold w-[50%] basisc-[25%]">
+                        ICT기반 정보시스템보안엔지니어 양성
+                    </h3>
+                    <span></span>
+                    <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
+                                <li>[ 웹 프로그래밍 기초 ] : PHP</li>
+                                <li>[ 네트워크 ] : switch, router, cisco</li>
+                                <li>[ 서버 ] : window server, centos, </li>
+                                <li>[ 데이터베이스 ] : MySQL</li>
+                                <li>[ 블록체인 ] : Solidity</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </Wrapper>
     )
 }
